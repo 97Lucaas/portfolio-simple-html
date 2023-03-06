e2922951f6ea81be74993346ceb74f12d5ecf73f
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2023-03-06 17:47:46 +0100

    some header stuff

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,22 +1,45 @@
 
-let elem = document.getElementById("overlay");
+let elem = document.getElementById("header");
 let open1 = document.getElementById("open");
 let close1 = document.getElementById("close");
+let textcont = document.getElementById('text-cont');
+let nav = document.getElementById('nav');
 
 close1.style.display = "none";
 
 function Open(){
 
-    elem.style.transform = "translateX(" + (0) + "px)";
+    //elem.classList.add("head-colapsed");
+    //elem.style.transform = "translateX(" + (0) + "px)";
+    textcont.style.display = "flex";
+
+    elem.style.height = "100vh";
     open1.style.display = "none";
     close1.style.display = "block";
+
+    nav.style.justifyContent = "center";
+    nav.style.alignSelf = "auto";
+
+    textcont.style.flexDirection = "column";
+    textcont.style.width = "auto";
 
 }
 
 function Close(){
 
-    elem.style.transform = "translateX(" + (110) + "%) translateY(-1000px)";
+    //elem.style.style.removeProperty('height');
+    elem.style.height = "112px";
+    //elem.classList.remove("head-colapsed");
     open1.style.display = "block";
     close1.style.display = "none";
+
+    nav.style.justifyContent = "flex-end";
+
+    textcont.style.flexDirection = "row";
+    textcont.style.width = "100vh";
+
+    textcont.style.display = "none";
+
+    nav.style.alignSelf = "top";
 
 }
